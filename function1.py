f16182f2c832b5d135eb5e7975928e89f56efd1b
lyoung92 <lyoung92@naver.com>
2024-05-07 17:50:35 +0900

    함수 연습 완료

--- function1.py
+++ function1.py
@@ -36,3 +36,33 @@ def connectURI(server, port) :
 
 print(connectURI("naver.com", "80"))    
 print(connectURI(port = "8080", server = "daum.net"))   # 순서 변경이 되어도 인자 이름으로 값 전달하여 문제 없음
+
+#가변 인자
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+def union2(*ar):
+    result = set()
+    for item in ar:
+        for x in item:
+            result.add(x)
+    return result
+
+print(union("HAM", "EGG"))
+print(union("HAM", "EGG", "SPAM"))
+
+print(union2("HAM", "EGG"))
+print(union2("HAM", "EGG", "SPAM"))
+
+#람다 함수 : 이름이 없고 함수 객체만 존재하는 익명 함수
+#문법 = [lambda 인자 : 구문]
+g = lambda x,y : x*y
+print(g(2,3))
+
+h = (lambda x : x*x)(3)
+print(h)
